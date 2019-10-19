9ca342fb29235627079cc920f51fbedfa251a349
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2019-10-19 18:26:33 -0300

    add sprite, animation, and player dead

--- sketch.js
+++ sketch.js
@@ -4,13 +4,19 @@ let keys = {
   rot: 0,
   fire: false,
   restart: true,
+  mouse_control: false,
 };
+
+let satelite_image;
+let nave_image;
+let lixo_images = [];
+let terra_image;
 
 class Satelite {
   constructor() {
     this.x = width/2;
     this.y = height/2;
-    this.life = 5;
+    this.life = 15;
   }
 
   hit(bullet) {
@@ -23,21 +29,30 @@ class Satelite {
   }
 
   hit_l(lixo) {
-    if (dist(this.x, this.y, lixo.x, lixo.y) < 32 + LIXO_RADIUS) {
-      this.life -= 1;
+    if (dist(this.x, this.y, lixo.x, lixo.y) < 32 + lixo.radius) {
+      this.life -= 3;
+      if (this.life < 0) this.life = 0;
       return true;
     }
     return false;
   }
 
   draw() {
-    fill(150);
     noStroke();
-    ellipse(this.x, this.y, 64);
+    
+    image(
+      satelite_image,
+      this.x - 32, this.y - 32,
+      64, 64,
+      this.life>0?64*(floor(time*5)%4):0, 64*floor((1 - this.life/15)*3),
+      64, 64
+    );
+
     fill(255,0,0);
-    rect(this.x - 32, this.y - 32 - 4, 64, 8);
+    rect(this.x - 24, this.y - 32 - 4, 48, 4);
+
     fill(0,255,0);
-    rect(this.x - 32, this.y - 32 - 4, 64*this.life/5.0, 8);
+    rect(this.x - 24, this.y - 32 - 4, 48*this.life/15.0, 4);
   }
 }
 
@@ -70,8 +85,6 @@ class Bullet {
   }
 }
 
-const LIXO_RADIUS = 15;
-
 class Lixo {
   constructor(x,y, vx, vy) {
     this.x = x;
@@ -79,6 +92,9 @@ class Lixo {
     this.vx = vx;
     this.vy = vy;
     this.life = 3;
+    this.sprite = floor(random(0, 4));
+    this.radius = this.sprite!=2? 16 : 8;
+    this.is_hit = 0;
   }
 
   update() {
@@ -87,28 +103,38 @@ class Lixo {
   }
 
   draw() {
-    fill(100,50,0);
-    stroke(255);
-    ellipse(this.x, this.y, LIXO_RADIUS*2);
+    // fill(100,50,0);
+    // stroke(255);
+    // ellipse(this.x, this.y, this.radius*2);
+    image(
+      lixo_images[this.sprite],
+      this.x - this.radius, this.y - this.radius,
+      this.radius*2, this.radius*2,
+      this.radius*2*(3 - this.life),  this.radius*2*(this.is_hit>0?1:0),
+      this.radius*2, this.radius*2
+    );
+    if(this.is_hit > 0) {
+      this.is_hit -= 1;
+    }
   }
 
   hit(bullet) {
-    if (dist(bullet.x, bullet.y, this.x, this.y) < LIXO_RADIUS + 10) {
+    if (dist(bullet.x, bullet.y, this.x, this.y) < this.radius) {
       this.life-=1;
       const MASS_RATIO = 1/30;
       this.vx += bullet.vx*MASS_RATIO;
       this.vy += bullet.vy*MASS_RATIO;
+      this.is_hit = 5;
       return true;
     }
     return false;
   }
 
   is_out_of_bounds() {
-    return (this.vx < 0 && this.x < -LIXO_RADIUS)
-      || (this.vx > 0 && this.x > width + LIXO_RADIUS)
-      || (this.vy < 0 && this.y < -LIXO_RADIUS)
-      || (this.vy > 0 && this.y > height + LIXO_RADIUS);
-
+    return (this.vx < 0 && this.x < -this.radius)
+      || (this.vx > 0 && this.x > width + this.radius)
+      || (this.vy < 0 && this.y < -this.radius)
+      || (this.vy > 0 && this.y > height + this.radius);
   }
 }
 
@@ -124,6 +150,7 @@ class Player {
     this.va = 0;
     this.fire_delay = 0.0;
     this.cannon = 0;
+    this.life = 3;
   }
 
   update() {
@@ -137,6 +164,12 @@ class Player {
     let sx = cos(this.a);
     let sy = sin(this.a);
 
+    if (keys.mouse_control) {
+      let mousev = mouseVel();
+      keys.accel = min(mousev.y / 12, 1.0);
+      keys.rot = min(mousev.x / 12, 1.0);
+    }
+
     if (keys.accel > 0) {
       this.vx += fx * ACCELL * dt;
       this.vy += fy * ACCELL * dt;
@@ -148,14 +181,13 @@ class Player {
       if (this.fire_delay <= 0.0) {
         const BULLET_VEL = 120;
 
-        let px = this.x + fx*10 + 10*sx*(this.cannon==0? 1 : -1);
-        let py = this.y + fy*10 + 10*sy*(this.cannon==0? 1 : -1);
+        let px = this.x + fx*10 + 23.5*sx*(this.cannon==0? 1 : -1);
+        let py = this.y + fy*10 + 23.5*sy*(this.cannon==0? 1 : -1);
 
         this.cannon = (this.cannon==0? 1 : 0);
 
         bullets.push(new Bullet(px ,py, fx*BULLET_VEL, fy*BULLET_VEL));
         this.fire_delay = FIRE_INT;
-        print("FIRE!!");
       }
     }
     this.fire_delay -= dt;
@@ -169,24 +201,59 @@ class Player {
       this.va = this.va>0? ROT_LIMIT : -ROT_LIMIT;
     }
   }
+  
+  hit(lixo) {
+    if(dist(lixo.x, lixo.y, this.x, this.y) < 32 + lixo.radius) {
+      this.life -= 1;
+      if (this.life <= 0) this.life = 0;
+
+      let dx = this.x - lixo.x;
+      let dy = this.y - lixo.y;
+
+      let dvx = this.vx - lixo.vx;
+      let dvy = this.vy - lixo.vy;
+
+      let norm = sqrt(dx**2 + dy**2);
+
+      dx = dx/norm;
+      dy = dy/norm;
+
+      let n = dvx*dx + dvy*dy;
+
+      this.vx -= 1.05*n*dx;
+      this.vy -= 1.05*n*dy;
+
+      lixo.vx += 1.05*n*dx;
+      lixo.vy += 1.05*n*dy;
+    }
+  }
 
   draw() {
-    fill(255);
-    stroke(0);
-    let fx = sin(this.a) * 20;
-    let fy = -cos(this.a) * 20;
+    // fill(255);
+    // stroke(0);
+    // let fx = sin(this.a) * 20;
+    // let fy = -cos(this.a) * 20;
 
-    let sx = cos(this.a) * 15;
-    let sy = sin(this.a) * 15;
+    // let sx = cos(this.a) * 15;
+    // let sy = sin(this.a) * 15;
 
-    triangle(
-      this.x + fx,
-      this.y + fy, 
-      this.x - fx + sx,
-      this.y - fy + sy,
-      this.x - fx - sx,
-      this.y - fy - sy
-    );
+    // triangle(
+    //   this.x + fx,
+    //   this.y + fy, 
+    //   this.x - fx + sx,
+    //   this.y - fy + sy,
+    //   this.x - fx - sx,
+    //   this.y - fy - sy
+    // );
+
+    let sx = !keys.accel? 0 : 1 + (floor(time*5)%2);
+
+    push();
+    translate(floor(this.x), floor(this.y));
+    rotate(this.a);
+    translate(-32, -36);
+    image(nave_image, 0, 0, 64, 72, sx*64, 0, 64, 72);
+    pop();
   }
 }
 
@@ -198,11 +265,28 @@ let bullets = [];
 let lixo_delay = 0.0;
 let time = 0.0;
 
+function preload() {
+  satelite_image = loadImage('assets/satelite.png');
+  nave_image = loadImage('assets/nave.png');
+  lixo_images[0] = loadImage('assets/lixo0.png');
+  lixo_images[1] = loadImage('assets/lixo1.png');
+  lixo_images[2] = loadImage('assets/lixo2.png');
+  lixo_images[3] = loadImage('assets/lixo3.png');
+  terra_image = loadImage('assets/terra.png');
+}
+
 function setup() {
   createCanvas(800, 600);
   background(10);
 
   init_new_game();
+}
+
+function mouseVel() {
+  return {
+    x: mouseX - pmouseX,
+    y: mouseY - pmouseY,
+  }
 }
 
 function keyPressed() {
@@ -225,6 +309,15 @@ function keyPressed() {
       break;
     case 82: //R
       keys.restart = true;
+      break;
+    case 77: //M
+      keys.mouse_control = !keys.mouse_control;
+      if(keys.mouse_control) {
+        requestPointerLock();
+      } else {
+        exitPointerLock();
+      }
+      mouse
       break;
   }
 }
@@ -259,10 +352,11 @@ function init_new_game() {
 
   bullets = [];
   lixos = [];
+
+  time = 0.0;
 }
 
 function draw() {
-  background(10);
 
   const SPAWN_INT = 3.0;
   const LIXO_VEL = 20;
@@ -282,6 +376,7 @@ function draw() {
   player.update();
   for (let i = lixos.length - 1; i >= 0; i--) {
     lixos[i].update();
+    player.hit(lixos[i]);
     if (lixos[i].is_out_of_bounds()) {
       lixos.splice(i,1);
       continue;
@@ -318,6 +413,10 @@ function draw() {
 
   //>> DRAW
 
+  background(10);
+
+  image(terra_image, floor(width/4) - 64, height/2 - 64, 128, 128, 128*(floor(time)%3), 0, 128, 128);
+
   for (let i = bullets.length - 1; i >= 0; i--) {
     bullets[i].draw();
   }
@@ -328,13 +427,14 @@ function draw() {
     lixos[i].draw();
   }
 
-  if (satelite.life <= 0) {
+  if (satelite.life <= 0 || player.life <= 0) {
     fill(255,0,0,40);
     rect(0, 0, width, height);
     textAlign(CENTER,CENTER);
     textSize(64);
     fill(255,0,0);
     stroke(0);
+    strokeWeight(3);
     text("YOU FAIL!!!!", width/2, height/2);
     textSize(32);
     text(`you protect the satellite for ${floor(time)} seconds`, width/2, height/2 + 64);
@@ -348,6 +448,7 @@ function draw() {
     stroke(0);
     text(`SCORE: ${floor(time)}`, 10, 10);
   }
+  strokeWeight(1);
 
   if (keys.restart) {
     keys.restart = false;
